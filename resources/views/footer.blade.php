7c0b055a00b49f1012fac0b2771f4f681c854fec
Gustavo Rogalewski <gustavo.rogalewski@bol.com.br>
2022-08-04 22:13:21 -0300

    footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -21,14 +21,35 @@
                             <a class="list-item" href="">Trabalhe Conosco</a>
                             <div class="contact">
                                 <i class="fas fa-headphones-alt footer-icon"></i>
-                                <span class="number"> (41) XXXX-XXXX</span>
+                                <span class="number"> (41) 0000-0000</span>
                             </div>
                             <div class="contact">
                                 <i class="fab fa-whatsapp footer-icon"></i>
-                                <span class="number"> (41) XXXX-XXXX</span>
+                                <span class="number"> (41) 0000-0000</span>
                             </div>
                     </div>
                 </div>
+            </div>
+        </div>
+        <hr>
+        <div class="social">
+            <p>FutStore Ltda CNPJ 00.000.000/0000-00 - Avenida do Batel, 1868 CEP: 80420-090 Curitiba-PR &copy; 2022</p>
+            <div>
+                <a href="#">
+                    <i class="fab fa-twitter midia"></i>
+                </a>
+                <a href="#">
+                    <i class="fab fa-facebook-f midia"></i>
+                </a>
+                <a href="#">
+                    <i class="fab fa-instagram midia"></i>
+                </a>
+                <a href="#">
+                    <i class="fab fa-whatsapp midia"></i>
+                </a>
+                <a href="#">
+                    <i class="fab fa-youtube midia"></i>
+                </a>
             </div>
         </div>
     </div>
