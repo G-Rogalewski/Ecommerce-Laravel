eacd9385844f7e6fd2e262ff085150d622fe7146
Gustavo Rogalewski <gustavo.rogalewski@bol.com.br>
2022-08-04 21:24:27 -0300

    footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,36 @@
 @section('footer')
-<footer class="bg-dark">footer</footer>
+<footer class="bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-lg-4 col-md-12 list">
+                        <h3>Institucional</h3>
+                            <a class="list-item" href="">Sobre Nós</a>
+                            <a class="list-item" href="">Compras Internacionais</a>
+                            <a class="list-item" href="">Blog</a>
+                    </div>
+                    <div class="col-lg-4 col-md-12 list">
+                        <h3>Ajuda</h3>
+                            <a class="list-item" href="">Troca e Devoçução</a>
+                            <a class="list-item" href="">Política de Privacidade</a>
+                            <a class="list-item" href="">Regulamentos</a>
+                    </div>
+                    <div class="col-lg-4 col-md-12 list">
+                        <h3>Contato</h3>
+                            <a class="list-item" href="">Trabalhe Conosco</a>
+                            <div class="contact">
+                                <i class="fas fa-headphones-alt footer-icon"></i>
+                                <span class="number"> (41) XXXX-XXXX</span>
+                            </div>
+                            <div class="contact">
+                                <i class="fab fa-whatsapp footer-icon"></i>
+                                <span class="number"> (41) XXXX-XXXX</span>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 @endsection
