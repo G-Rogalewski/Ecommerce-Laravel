eb3246daa2bfa9302177761fec0bfe94cc747960
Gustavo Rogalewski <gustavo.rogalewski@bol.com.br>
2022-08-09 12:48:16 -0300

    products

--- resources/views/clubes_paranaenses.blade.php
+++ resources/views/clubes_paranaenses.blade.php
@@ -97,6 +97,16 @@
                 </div>
                 <div class="col-lg-3 col-md-12">
                     <div class="card">
+                        <img class="card-img" src="{{ asset('images/brasileiros/parana/cascavel_2021_ii.png')}}">
+                        <h4 class="product-name">Camisa FC Casvavel 2021 - Away</h4>
+                        <p class="product-price">R$49,90</p>
+                        <div class="product-button">
+                            <button class="add-button"><i class="fas fa-cart-plus"></i> Adicionar</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-12">
+                    <div class="card">
                         <img class="card-img" src="{{ asset('images/brasileiros/parana/cambara_2018_i.png')}}">
                         <h4 class="product-name">Camisa Cambará 2018 - Home</h4>
                         <p class="product-price">R$99,90</p>
@@ -120,6 +130,36 @@
                         <img class="card-img" src="{{ asset('images/brasileiros/parana/maringa_2020_i.png')}}">
                         <h4 class="product-name">Camisa Maringá 2020 - <br/>Home</h4>
                         <p class="product-price">R$99,90</p>
+                        <div class="product-button">
+                            <button class="add-button"><i class="fas fa-cart-plus"></i> Adicionar</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-12">
+                    <div class="card">
+                        <img class="card-img" src="{{ asset('images/brasileiros/parana/cascavel_cr_2021_i.png')}}">
+                        <h4 class="product-name">Camisa Cascavel CR 2021 - Home</h4>
+                        <p class="product-price">R$119,90</p>
+                        <div class="product-button">
+                            <button class="add-button"><i class="fas fa-cart-plus"></i> Adicionar</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-12">
+                    <div class="card">
+                        <img class="card-img" src="{{ asset('images/brasileiros/parana/cascavel_cr_2021_ii.png')}}">
+                        <h4 class="product-name">Camisa Cascavel CR 2021 - Away</h4>
+                        <p class="product-price">R$119,90</p>
+                        <div class="product-button">
+                            <button class="add-button"><i class="fas fa-cart-plus"></i> Adicionar</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-12">
+                    <div class="card">
+                        <img class="card-img" src="{{ asset('images/brasileiros/parana/cascavel_cr_2021_iii.png')}}">
+                        <h4 class="product-name">Camisa Cascavel CR 2021 - Third</h4>
+                        <p class="product-price">R$119,90</p>
                         <div class="product-button">
                             <button class="add-button"><i class="fas fa-cart-plus"></i> Adicionar</button>
                         </div>
