29011c679e4f6ef1a68f3197cc1c202fa76c22f3
Gustavo Rogalewski <gustavo.rogalewski@bol.com.br>
2022-09-12 16:03:39 -0300

    fix

--- resources/views/clubes_brasileiros/clubes_paranaenses.blade.php
+++ resources/views/clubes_brasileiros/clubes_paranaenses.blade.php
@@ -3,7 +3,26 @@
 @section('products')
 @section('title', 'Clubes Paranaenses')
     <section>
-        teste
+        <div class="container">
+            <div class="row">
+                @if(isset($lista))
+                    @foreach($lista as $prod)
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="card">
+                                <img class="card-img" src="{{ $prod->foto }}">
+                                <h4 class="product-name">{{ $prod->nome }}</h4>
+                                <p class="product-price">R${{ $prod->valor }}</p>
+                                <div class="product-button">
+                                    <a href="{{ route('adicionar_carrinho', ['idproduto' => $prod->id]) }}">
+                                        <button class="add-button"><i class="fas fa-cart-plus"></i> Adicionar</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
+            </div>
+        </div>
     </section>
 @endsection
 @extends('footer')
